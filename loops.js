ea849515148a1ac7449236b8d8a11a00681f3cb8
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 14:40:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@
 function forLoop(array) {
   for (i = 0; i < 25; i++) {
     if (i == 1) {
-      console.log("I am" ${i} "strange loop");
+      console.log("I am ${i}strange loop");
     }
-    else console.log("I am" ${i} "strange loops");
+    else console.log("I am ${i} strange loops");
   }
 }
 
