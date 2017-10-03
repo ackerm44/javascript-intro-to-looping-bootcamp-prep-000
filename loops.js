089e006ba723715cef05e2bda5e4ed09c0c10c76
Amanda Ackerman <ackerm44@gmail.com>
2017-10-03 14:13:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 var array = [];
 
 for (i = 0; i < 25; i++) {
-  if (i = 1) {
+  if (i == 1) {
     console.log("I am ${i} strange loop");
   }
-  else console.log("I am ${i} strange loops"); 
+  else console.log("I am ${i} strange loops");
 }
